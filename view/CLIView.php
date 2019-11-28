25ee21578eea66f74412b9a5833ef9d8c1a6e933
Maël Querré <mael.querre@gmail.com>
2019-11-28 15:07:29 +0100

    Add clear console

--- view/CLIView.php
+++ view/CLIView.php
@@ -10,6 +10,8 @@ class CLIView extends Renderer
      */
     public function render($map)
     {
+        echo "\e[H\e[2J"; // clear console
+
         foreach ($map as $row) {
             foreach ($row as $col) {
                 echo "$col ";
